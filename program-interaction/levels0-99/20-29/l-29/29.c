d5d0fc31b28a896283417aa3c85a6837a3540a84
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-07-12 23:09:25 -0400

    solved using chatgpt for the bulk of the problem ... need to revisit.

---
+++ program-interaction/levels0-99/20-29/l-29/29.c
@@ -0,0 +1,25 @@
+#include <stdio.h>      // For standard input and output functions
+#include <stdlib.h>     // For standard library functions
+
+// Including these removes some of the additional warnings that we recieve in the problem.
+#include <unistd.h>     // For POSIX operating system API (execve, fork)
+#include <sys/wait.h>   // For wait function
+
+void pwncollege () {
+    char *argv[] = {"/challenge/embryoio_level29", NULL};  // Argument vector
+    execve("/challenge/embryoio_level29", argv, NULL);
+    exit(0);
+}
+
+int main (int argc, char argv[]) {
+	pid_t cpid;
+
+	if (fork() == 0) {
+		pwncollege();
+	}
+	else {
+		cpid = wait(NULL);
+	}
+
+	return 0;
+}
