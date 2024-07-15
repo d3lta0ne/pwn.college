2f71c0d6f7b36d4f585e8f9ca29e073f40acbe24
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-07-15 11:10:44 -0400

    update

--- program-interaction/levels0-99/20-29/l-29/29.c
+++ program-interaction/levels0-99/20-29/l-29/29.c
@@ -1,23 +1,27 @@
-#include <stdio.h>      // For standard input and output functions
-#include <stdlib.h>     // For standard library functions
+#include <stdio.h>	// For standard input and output functions
+#include <stdlib.h> // For standard library functions
 
 // Including these removes some of the additional warnings that we recieve in the problem.
-#include <unistd.h>     // For POSIX operating system API (execve, fork)
-#include <sys/wait.h>   // For wait function
+// #include <unistd.h>	  // For POSIX operating system API (execve, fork)
+// #include <sys/wait.h> // For wait function
 
-void pwncollege () {
-    char *argv[] = {"/challenge/embryoio_level29", NULL};  // Argument vector
-    execve("/challenge/embryoio_level29", argv, NULL);
-    exit(0);
+void pwncollege()
+{
+	char *argv[] = {"/challenge/embryoio_level29", NULL}; // Argument vector
+	execve("/challenge/embryoio_level29", argv, NULL);
+	exit(0);
 }
 
-int main (int argc, char argv[]) {
+int main(int argc, char argv[])
+{
 	pid_t cpid;
 
-	if (fork() == 0) {
+	if (fork() == 0)
+	{
 		pwncollege();
 	}
-	else {
+	else
+	{
 		cpid = wait(NULL);
 	}
 
