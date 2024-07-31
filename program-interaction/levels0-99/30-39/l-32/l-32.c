7b57ee8b7e9ed3c385bce9548630029740caf133
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-07-31 19:45:05 -0400

    Problem started, found how to set environemntal variables in C, using approach 1

---
+++ program-interaction/levels0-99/30-39/l-32/l-32.c
@@ -0,0 +1,15 @@
+#include <stdio.h>      // For standard input/output functions
+#include <stdlib.h>     // For exit(), NULL, setenv()
+#include <unistd.h>     // For fork(), execve(), getpid(), etc.
+#include <sys/types.h>  // For data types used in system calls (e.g., pid_t)
+#include <sys/wait.h>   // For wait() and related macros
+
+void pwncollege()
+{
+
+}
+
+int main (int argc, char argv[])
+{
+
+}
