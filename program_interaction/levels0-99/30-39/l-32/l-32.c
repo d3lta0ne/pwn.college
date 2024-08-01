05102e018b6f68d22ee82830b71b3015159be6a6
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-08-01 04:20:58 -0400

    solved l-32 using option 1

--- program_interaction/levels0-99/30-39/l-32/l-32.c
+++ program_interaction/levels0-99/30-39/l-32/l-32.c
@@ -1,15 +1,33 @@
 #include <stdio.h>      // For standard input/output functions
 #include <stdlib.h>     // For exit(), NULL, setenv()
-#include <unistd.h>     // For fork(), execve(), getpid(), etc.
+#include <unistd.h>     // For fork(), execve(), getpid()
 #include <sys/types.h>  // For data types used in system calls (e.g., pid_t)
 #include <sys/wait.h>   // For wait() and related macros
 
+// Option 1: 
+// using the setenv() and environ variable
+
 void pwncollege()
 {
-
+    extern char **environ;
+    char *argv[] = {"/challenge/embryoio_level32", NULL};
+    setenv("mrsqev", "oaxcmkzbmf", 1);
+    execve("/challenge/embryoio_level32", argv, NULL);
+    exit(0);
 }
 
 int main (int argc, char argv[])
 {
+    pid_t cpid;
 
+    if (fork() == 0)
+    {
+        pwncollege();
+    }
+    else
+    {
+        cpid = wait(NULL);
+    }
+
+    return 0;
 }
