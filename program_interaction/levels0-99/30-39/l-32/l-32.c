9c097017ce6c710813ba7aa18964aceb61edca7b
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-08-01 03:41:36 -0400

    reoganized

---
+++ program_interaction/levels0-99/30-39/l-32/l-32.c
@@ -0,0 +1,24 @@
+#include <stdio.h>      // For standard input/output functions
+#include <stdlib.h>     // For exit(), NULL
+#include <unistd.h>     // For fork(), execve(), getpid(), etc.
+#include <sys/types.h>  // For data types used in system calls (e.g., pid_t)
+#include <sys/wait.h>   // For wait() and related macros
+
+void pwncollege () {
+    char *argv[] = {"/challenge/embryoio_level32", NULL};
+    execve("/challenge/embryoio_level32", argv, NULL);
+    exit(0);
+}
+
+int main (int argc, char argv[]){
+    pid_t cpid;
+
+    if(fork()==0){
+        pwncollege();
+    }
+    else{
+        cpid=wait(NULL);
+    }
+
+    return 0;
+}
